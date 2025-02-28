362d4482e8fc3a1a28f5cc0d5259a47e51aa2eb0
Sumit Kumar <sumitkumar@kgpian.iitkgp.ac.in>
2025-02-28 16:09:36 +0530

    Update AddCitizenToHousehold.tsx

--- src/components/employee/AddCitizenToHousehold.tsx
+++ src/components/employee/AddCitizenToHousehold.tsx
@@ -95,8 +95,8 @@ export default function AddCitizenToHousehold({
                   required
                 >
                   <option value="">Select Gender</option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
                   <option value="Other">Other</option>
                 </select>
               </div>
